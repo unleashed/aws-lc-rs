a60662faf0ce41f66ec816e30f73c75c5748697f
Alejandro Martinez Ruiz <amr@buoyant.io>
2025-12-01 22:26:02 +0100

    aws-lc-fips-sys: error out if not using bindgen generated bindings

--- aws-lc-fips-sys/src/lib.rs
+++ aws-lc-fips-sys/src/lib.rs
@@ -65,6 +65,8 @@ mod generated {
 }
 #[cfg(use_bindgen_generated)]
 pub use generated::*;
+#[cfg(not(use_bindgen_generated))]
+compile_error!{"no use bindgen"}
 
 #[allow(non_snake_case)]
 #[must_use]
